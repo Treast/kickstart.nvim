c6ca30172d0a481ef28461b46ffebd15ee9505df
Treast <vincent.riva@outlook.fr>
2024-05-24 21:27:52 +0200

    Add Treesitter & NERDTree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,10 +3,27 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    }
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      local configs = require 'nvim-treesitter.configs'
+
+      configs.setup {
+        ensure_installed = { 'php', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'go', 'twig', 'html', 'twig', 'rust' },
+        sync_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+        auto_install = true,
+      }
+    end,
+  },
+  { 'folke/neodev.nvim', opts = {} },
+  { 'preservim/nerdtree' },
 }
